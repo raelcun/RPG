0c689191c10223201289fb2e66bc449336599b6b
TheSauceror <piattjacob@gmail.com>
2014-07-06 12:08:46 -0400

    Fix'd the NEW MESSAGE display

--- checklogin.php
+++ checklogin.php
@@ -4,7 +4,6 @@ if(isset($_COOKIE["PHPRPG"])) {
   $cookie = explode("||",$_COOKIE["PHPRPG"]);
   $conn=mysqli_connect("ucfsh.ucfilespace.uc.edu","piattjd","curtis1","piattjd");
   $hero = mysqli_fetch_assoc(mysqli_query($conn,"SELECT pw FROM Hero WHERE name = '$cookie[0]'"));
-  mysqli_close($conn);
   if($cookie[1] == $hero[pw]) {
     echo "<a href='home.php'>$cookie[0]</a> - ";
     echo "<a href='herolist.php'>Hero List</a> - ";
@@ -15,12 +14,14 @@ if(isset($_COOKIE["PHPRPG"])) {
     echo "<a href='market.php'>Market</a> - ";
     echo "<a href='battleplan.php'>Battle Plan</a> - ";
     if(!is_null(mysqli_fetch_assoc(mysqli_query($conn,"SELECT * FROM Messages WHERE receiver = '$cookie[0]' AND unread = 1")))) { echo "<a href='messages.php'>NEW MESSAGES</a> - "; } else { echo "<a href='messages.php'>Messages</a> - "; };
+    mysqli_close($conn);
     echo "<a href='logout.php'>Logout</a><br>";
     echo "<br>";
     return;
   }
 }
 else {
+  mysqli_close($conn);
   header('Location: login.php');
 }
 ?>
