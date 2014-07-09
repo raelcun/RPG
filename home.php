4a2db3c8050d196d1c930ed6962de347580c8f1e
TheSauceror <piattjacob@gmail.com>
2014-07-09 00:42:17 -0400

    Ability List

--- home.php
+++ home.php
@@ -8,6 +8,7 @@ echo "<a href='loadhero.php'>Load Hero</a><br>";
 echo "<a href='loadparty.php'>Load Party</a><br>";
 echo "<a href='itemlist.php'>Item List</a><br>";
 echo "<a href='loaditem.php'>Load Item</a><br>";
+echo "<a href='abilitylist.php'>Ability List</a><br>";
 echo "<a href='market.php'>Market</a><br>";
 echo "<a href='battleplan.php'>Battle Plan</a><br>";
 echo "<a href='messages.php'>Messages</a><br>";
